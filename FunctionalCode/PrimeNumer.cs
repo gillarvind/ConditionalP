ac0be941ee182759b43b99c28dd68a2ef69c786e
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-29 14:54:54 +0530

    [arvind] Add: branch3 created

---
+++ FunctionalCode/PrimeNumer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class PrimeNumer
+    {
+        int count = 0;
+        public void Prime()
+        {
+            Console.WriteLine("check number is prime number or not");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine("this is a prime number");
+                }
+                else
+                {
+                    Console.WriteLine("this is not a prime number");
+                }
+            }
+        }
+    }
+}
