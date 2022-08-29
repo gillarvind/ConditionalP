e500a679d492361dce8ae34858e89f1dcd407738
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-29 14:50:18 +0530

    [arvind] Add: branch2 created

---
+++ FunctionalCode/ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class ReverseNumber
+    {
+        int reverse = 0, rem;
+        public void RevNumber()
+        {
+            Console.WriteLine("enter a number to reverse");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                rem = num % 10;
+                reverse = (reverse * 10) + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("reverse number is:" + reverse);
+        }
+    }
+}
