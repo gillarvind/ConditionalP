57988736ab869501cc3a15632c6c8f9e14569dfb
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-29 14:41:27 +0530

    [arvind] Add: branch1 created

---
+++ FunctionalCode/ConversionP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCode
+{
+    internal class ConversionP
+    {
+        double f, c;
+        public void Conver()
+        {
+            Console.WriteLine("choose any one for conversion\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("enter fahrenheit temp", f);
+                    f = Convert.ToDouble(Console.ReadLine());
+                    c = (double)(f - 32) * 5 / 9;
+                    Console.WriteLine("Celsius tem:" + c);
+                    break;
+                case 2:
+                    Console.WriteLine("enter celsius temp", c);
+                    c = Convert.ToDouble(Console.ReadLine());
+                    f = (double)((9 * c) / 5) + 32;
+                    Console.WriteLine("Fahrenheit temp:" + f);
+                    break;
+                default:
+                    Console.WriteLine("enter valid number");
+                    break;
+            }
+        }
+    }
+}
--- FunctionalCode/Program.cs
+++ FunctionalCode/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("select program \n1.Conversion Program\n2.ReverseNumber Program\n3.Prime Number Porgram\n4.Fibonacci Series Program\n5.\n5.Perfect Number Program");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        ConversionP conversionP = new ConversionP();
+                        conversionP.Conver();
+                        break;
+                    case 2:
+                        ReverseNumber reverseNumber = new ReverseNumber();
+                        reverseNumber.RevNumber();
+                        break;
+                    case 3:
+                        PrimeNumer primeNumer = new PrimeNumer();
+                        primeNumer.Prime();
+                        break;
+                    case 4:
+                        PerfectNum perfectNum = new PerfectNum();
+                        perfectNum.Number();
+                        break;
+                    case 5:
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                        fibonacciSeries.Series();
+                        break;
+                    default:
+                        Console.WriteLine("enter correct number");
+                        break;
+                }
+            }
         }
+    }
     }
 }
